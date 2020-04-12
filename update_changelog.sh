76b967b6ea5a985bbc19df3e6f99a57ede3efcec
Chris Maillefaud <chrismaillefaud@gmail.com>
2020-04-12 12:56:03 -0300

    fix: small fixes in deploy process

--- update_changelog.sh
+++ update_changelog.sh
@@ -47,25 +47,26 @@ echo "Bump rule: ${version_rule}"
 # Update Version
 poetry version "${version_rule}"
 
-# Update Changelog
 PACKAGE_NEW_VERSION=$(poetry version | grep -o -P "(?<=stela )\S+")
 echo "New version: ${PACKAGE_NEW_VERSION}"
-auto-changelog -v ${PACKAGE_NEW_VERSION}
-
-# Commit alterations
-echo "Commiting alterations..."
-git config --global user.email "chrismaillefaud@gmail.com"
-git config --global user.name "Chris Maillefaud"
-git fetch --all
-git add CHANGELOG.md
-git add pyproject.toml
-git commit -m "[skip-ci] auto-bump version ${PACKAGE_NEW_VERSION}"
-git push "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git" "${current_branch}"
 
 # Tag new Version
 if [[ ${current_branch} == "master" ]]
 then
 	git tag ${PACKAGE_NEW_VERSION}
-	git push "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git" "${current_branch}" --tags
 fi
+
+# Update Changelog
+auto-changelog
+
+# Commit alterations
+echo "Commiting alterations..."
+git config --global user.email ${GIT_EMAIL}
+git config --global user.name ${GIT_NAME}
+git fetch --all
+git add CHANGELOG.md
+git add pyproject.toml
+git commit -m "[skip-ci] auto-bump version ${PACKAGE_NEW_VERSION}"
+git push "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git" "${current_branch}" --tags
+
 

b692461e3712a39b0d21c144f5364a156224c1cf
Chris Maillefaud <chrismaille@users.noreply.github.com>
2023-08-14 18:06:40 -0300

    fix: fix versions

--- stela/__init__.py
+++ stela/__init__.py
@@ -18,7 +18,7 @@ from stela.main.cut import StelaCut, StelaCutMain
 from stela.main.dot import StelaDotMain
 from stela.utils import find_file_folder, show_value
 
-__version__ = "5.0.4"
+__version__ = "5.1.1"
 
 
 def _get_stela_cut() -> StelaCutMain:
